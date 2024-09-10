1ea1b90122a690993aee309abfbcc5c8b424f529
KaiqueVA <Kaique_x_@hotmail.com>
2024-09-10 16:39:39 -0300

    Add LoRaWAN header file

---
+++ PrototipoContentores/lib/LoRaWAN/LoRaWAN.h
@@ -0,0 +1,13 @@
+#ifndef LORAWAN_H
+#define LORAWAN_H
+
+#include <lmic.h>
+#include <hal/hal.h>
+
+void init_lorawan(u1_t *NWKSKEY, size_t nwkskey_size, u1_t *APPSKEY, size_t appskey_size, u4_t DEVADDR);
+void os_getArtEui (u1_t* buf);
+void os_getDevEui (u1_t* buf);
+void os_getDevKey (u1_t* buf);
+void send_data(char *data, uint8_t *size);
+
+#endif
