7aa25cdd0d78566ab52279df6ffef9d663a7ea07
KaiqueVA <Kaique_x_@hotmail.com>
2024-09-10 16:54:41 -0300

    Refactor main.cpp to include data_processing.h and remove unnecessary code

--- PrototipoContentores/src/main.cpp
+++ PrototipoContentores/src/main.cpp
@@ -1,11 +1,10 @@
-/*
-#include "Arduino.h"
-*/
-#include "defines.h"
 #include <Arduino.h>
+
+#include "defines.h"
 #include "LoRaWAN.h"
 #include "sensors_contentor.h"
 #include "deep_sleep.h"
+#include "data_processing.h"
 
 
 
@@ -17,11 +16,6 @@ const lmic_pinmap lmic_pins = {
     .dio = {RADIO_DIO0_PORT, RADIO_DIO1_PORT, RADIO_DIO2_PORT},
 };
 
-
-
-
-
-void dataProcessing(TinyGPSPlus *gps, VL53L0X *sensor, char **p_dados, uint8_t *tamanhoStr);
 
 
 void setup() 
@@ -57,41 +51,3 @@ void setup()
 }
 
 void loop() {}
-
-
-
-
-
-
-
-
-void dataProcessing(TinyGPSPlus *gps, VL53L0X *sensor, char **p_dados, uint8_t *tamanhoStr)
-{
-    uint8_t tombo = digitalRead(INCLINACAO);
-    uint8_t batery = leituraBat();
-    uint16_t volume = leituraSensor(sensor);
-    double latitude = 0;
-    double longitude = 0;
-
-    getGPS(gps, &latitude, &longitude);
-
-    *tamanhoStr = snprintf(nullptr, 0, "%03d%04d%03.6f%03.6f%d", batery, volume, latitude, longitude, tombo);
-    *p_dados = (char*)malloc((*tamanhoStr + 1) * sizeof(char));
-    if(*p_dados == nullptr)
-    {
-        Serial.println("Erro ao alocar memoria");
-        delay(1000);
-        ESP.restart();
-    }
-    else
-    {
-        snprintf(*p_dados, *tamanhoStr + 1, "%03d%04d%03.6f%03.6f%d", batery, volume, latitude, longitude, tombo);
-        Serial.println(*p_dados);
-    }
-
-}
-
-
-
-
-
