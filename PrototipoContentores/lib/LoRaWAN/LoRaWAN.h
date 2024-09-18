ac8455142ba65f92b9820aa05d148b9dad154df3
KaiqueVA <Kaique_x_@hotmail.com>
2024-09-18 16:51:56 -0300

    Add verifyTransmition function to LoRaWAN library

--- PrototipoContentores/lib/LoRaWAN/LoRaWAN.h
+++ PrototipoContentores/lib/LoRaWAN/LoRaWAN.h
@@ -3,11 +3,13 @@
 
 #include <lmic.h>
 #include <hal/hal.h>
+#include "stdbool.h"
 
 void init_lorawan(u1_t *NWKSKEY, size_t nwkskey_size, u1_t *APPSKEY, size_t appskey_size, u4_t DEVADDR);
 void os_getArtEui (u1_t* buf);
 void os_getDevEui (u1_t* buf);
 void os_getDevKey (u1_t* buf);
+uint32_t verifyTransmition();
 void send_data(char *data, uint8_t *size);
 
 #endif
