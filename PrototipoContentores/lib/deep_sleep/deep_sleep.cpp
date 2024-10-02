aa4706ef1dd6090c5d48f621a14117a031a46338
KaiqueVA <kaique_x_@hotmail.com>
2024-10-02 14:38:33 -0300

    Refactor deep_sleep.cpp to add rtc_wockUp variable and implement gpsSleep and verifyWokeUpTimes functions

--- PrototipoContentores/lib/deep_sleep/deep_sleep.cpp
+++ PrototipoContentores/lib/deep_sleep/deep_sleep.cpp
@@ -2,10 +2,31 @@
 #include "defines.h"
 #include <Arduino.h>
 
+RTC_DATA_ATTR uint8_t rtc_wockUp = 1;
+
+bool verifyWokeUpTimes(void)
+{
+    if(rtc_wockUp == 1){
+        rtc_wockUp++;
+        return true;
+    }
+    
+    rtc_wockUp++;
+    if(rtc_wockUp > 8)
+        rtc_wockUp = 1;
+    return false;
+}
+
 void espSleep(uint32_t minutesSleep)
 {
     esp_sleep_enable_timer_wakeup(minutesSleep * 60 * uS_TO_S_FACTOR);
 
     Serial.println("Dormindo...");
     esp_deep_sleep_start();
+}
+
+void gpsSleep(void)
+{
+    digitalWrite(GPS_SLEEP, LOW);
+    Serial1.print("$PMTK161,0*28\r\n");
 }
