3293720d18508a5483e61cd27226263577c9282a
KaiqueVA <kaique_x_@hotmail.com>
2024-10-02 14:36:51 -0300

    Refactor data_processing.cpp to improve data processing and transmission

--- PrototipoContentores/lib/data processing/data_processing.cpp
+++ PrototipoContentores/lib/data processing/data_processing.cpp
@@ -1,17 +1,14 @@
 #include "data_processing.h"
 #include "defines.h"
 
-void dataProcessing(TinyGPSPlus *gps, VL53L0X *sensor, char **p_dados, uint8_t *tamanhoStr)
+void dataProcessing( VL53L0X *sensor, gps_data_t *gps_data, char **p_dados, uint8_t *tamanhoStr)
 {
     uint8_t tombo = digitalRead(INCLINACAO);
     uint8_t batery = leituraBat();
     uint16_t volume = leituraSensor(sensor);
-    double latitude = 0;
-    double longitude = 0;
+    
 
-    getGPS(gps, &latitude, &longitude);
-
-    *tamanhoStr = snprintf(nullptr, 0, "%03d%04d%03.6f%03.6f%d", batery, volume, latitude, longitude, tombo);
+    *tamanhoStr = snprintf(nullptr, 0, "%03d%04d%03.6f%03.6f%d", batery, volume, gps_data->latitude, gps_data->longitude, tombo);
     *p_dados = (char*)malloc((*tamanhoStr + 1) * sizeof(char));
     if(*p_dados == nullptr)
     {
@@ -21,8 +18,35 @@ void dataProcessing(TinyGPSPlus *gps, VL53L0X *sensor, char **p_dados, uint8_t *
     }
     else
     {
-        snprintf(*p_dados, *tamanhoStr + 1, "%03d%04d%03.6f%03.6f%d", batery, volume, latitude, longitude, tombo);
+        snprintf(*p_dados, *tamanhoStr + 1, "%03d%04d%03.6f%03.6f%d", batery, volume, gps_data->latitude, gps_data->longitude, tombo);
         Serial.println(*p_dados);
     }
 
 }
+
+void dataProcessing_no_gps(VL53L0X *sensor, gps_data_t *gps_data, char **p_dados, uint8_t *tamanhoStr)
+{
+    uint8_t tombo = digitalRead(INCLINACAO);
+    uint8_t batery = leituraBat();
+    uint16_t volume = leituraSensor(sensor);
+
+    
+
+    *tamanhoStr = snprintf(nullptr, 0, "%03d%04d%03.6f%03.6f%d", batery, volume, gps_data->latitude, gps_data->longitude, tombo);
+    *p_dados = (char*)malloc((*tamanhoStr + 1) * sizeof(char));
+    if(*p_dados == nullptr)
+    {
+        Serial.println("Erro ao alocar memoria");
+        delay(1000);
+        ESP.restart();
+    }
+    else
+    {
+        snprintf(*p_dados, *tamanhoStr + 1, "%03d%04d%03.6f%03.6f%d", batery, volume, gps_data->latitude, gps_data->longitude, tombo);
+        Serial.println(*p_dados);
+    }
+
+}
+
+
+
