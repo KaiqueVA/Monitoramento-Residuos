8fd14bb9b3961eff79f4b28722a8bbf31d4a844e
KaiqueVA <Kaique_x_@hotmail.com>
2024-10-03 15:07:56 -0300

    Refactor message_handler.py to verify devAddr before processing message

--- Script MQTT/message_handler.py
+++ Script MQTT/message_handler.py
@@ -2,16 +2,31 @@ import json
 from utils import hex_to_ascii, decode
 
 def process_message(payload_str):
-    """Processa a mensagem recebida em formato JSON e traduz o campo 'data'."""
     try:
         payload_json = json.loads(payload_str)
-        value_devaddr = payload_json.get("devaddr", "")
-        hex_data = payload_json.get("data", "")
-        if hex_data:
-            ascii_resultado = hex_to_ascii(hex_data)
-            decoded_message = decode(ascii_resultado, value_devaddr)
-            print(f"Mensagem decodificada: {decoded_message}")
-        else:
-            print("Campo 'data' não encontrado na mensagem.")
+        if verify_devAddr(payload_str, "F3E35972"):
+            value_devaddr = payload_json.get("devaddr", "")
+            value_rssi = payload_json.get("rssi", "")
+            hex_data = payload_json.get("data", "")
+            if hex_data:
+                ascii_resultado = hex_to_ascii(hex_data)
+                # print(f"Key data: {ascii_resultado}")
+                decoded_message = decode(ascii_resultado, value_devaddr, value_rssi)
+                print(f"Mensagem decodificada: {decoded_message}")
+            else:
+                print("Campo 'data' não encontrado na mensagem.")
+        
+        
     except json.JSONDecodeError:
         print("Erro ao decodificar a mensagem JSON.")
+
+def verify_devAddr(message, devaddr):
+    try:
+        payload_json = json.loads(message)
+        value_devaddr = payload_json.get("devaddr", "")
+        if value_devaddr == devaddr:
+            return True
+        else:
+            return False
+    except json.JSONDecodeError:
+        return False
