2dbe5e9b1e6a49070a881a4adbbc49c4fa0e7f90
KaiqueVA <Kaique_x_@hotmail.com>
2024-10-03 15:07:39 -0300

    feat: function create table

--- Script MQTT/main.py
+++ Script MQTT/main.py
@@ -1,10 +1,10 @@
 import paho.mqtt.client as mqtt
 from message_handler import process_message
+from data_base import create_table
 
 TOPIC = "LORAWAN"
 
 def on_connect(client, userdata, flags, rc):
-    """Callback executado quando o cliente se conecta ao broker."""
     if rc == 0:
         print(f"Conectado com sucesso ao broker! Código de retorno: {rc}")
         client.subscribe(TOPIC)
@@ -13,12 +13,12 @@ def on_connect(client, userdata, flags, rc):
         print(f"Falha ao conectar. Código de retorno: {rc}")
 
 def on_message(client, userdata, msg):
-    """Callback executado quando uma mensagem é recebida no tópico."""
     payload_str = msg.payload.decode("utf-8")
+    # print(f"Mensagem completa recebida: {payload_str}")
     
     process_message(payload_str)
 
-def run_mqtt_client(broker_address="0.0.0.0", port=1883, keepalive=60):
+def run_mqtt_client(broker_address="52.1.135.87", port=1883, keepalive=60):
     """Função para inicializar e rodar o cliente MQTT."""
     client = mqtt.Client()
 
@@ -30,4 +30,5 @@ def run_mqtt_client(broker_address="0.0.0.0", port=1883, keepalive=60):
     client.loop_forever()
 
 if __name__ == "__main__":
+    create_table()
     run_mqtt_client()
