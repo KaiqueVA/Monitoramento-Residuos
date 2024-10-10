9d102d9d27190ab1693a41040cb7eb6e2e59c761
KaiqueVA <Kaique_x_@hotmail.com>
2024-10-10 15:40:16 -0300

    Refactor utils.py to include HTTP POST request for sending decoded data to API

--- Script MQTT/utils.py
+++ Script MQTT/utils.py
@@ -2,6 +2,9 @@ import json
 from decimal import Decimal
 from datetime import datetime
 from data_base import insert_data_to_db
+import requests
+
+URL = "http://172.16.51.83:8000/api/contentors/"
 
 def hex_to_ascii(hex_str):
     try:
@@ -20,14 +23,13 @@ def decode(message, devaddr, rssi):
     is_tipped_over = message[27] == '1'
 
     command = {
-        "DeviceAddr": devaddr,
-        "BatteryLevel": battery,
-        "Volume": (volume/10),
-        "IsTippedOver": is_tipped_over,
-        "Latitude": latitude,
-        "Longitude": longitude,
+        "device_addr": devaddr,
+        "battery_level": battery,
+        "volume": (volume/10),
+        "is_tipped_over": is_tipped_over,
+        "latitude": latitude,
+        "longitude": longitude,
         "timestamp": datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
-        "rssi": rssi
     }
     
     insert_data_to_db(
@@ -40,6 +42,14 @@ def decode(message, devaddr, rssi):
         timestamp=command["timestamp"], 
         rssi=rssi
     )
+    
+    try:
+        headers = {'Content-Type': 'application/json'}
+        response = requests.post(URL, data=json.dumps(command), headers=headers)  # Envia o payload como JSON
+        response.raise_for_status()
+        print("Dados enviados com sucesso para a API.")
+    except requests.exceptions.RequestException as e:
+        print(f"Erro ao enviar dados para a API: {e} Response: {response.text}")
 
     
     json_data = json.dumps(command)
