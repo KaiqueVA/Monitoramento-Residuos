6fa1a5745bba1df1d520e0a901e380ddfbc5d2b7
KaiqueVA <Kaique_x_@hotmail.com>
2024-10-18 17:09:24 -0300

    Refactor deep_sleep.cpp to include additional sleep modes and libraries

--- PrototipoContentores/lib/deep_sleep/deep_sleep.cpp
+++ PrototipoContentores/lib/deep_sleep/deep_sleep.cpp
@@ -1,6 +1,10 @@
 #include "deep_sleep.h"
 #include "defines.h"
 #include <Arduino.h>
+#include <driver/adc.h>
+#include <esp_sleep.h>
+#include <SPI.h>
+#include <Wire.h>	
 
 RTC_DATA_ATTR uint8_t rtc_wockUp = 1;
 
@@ -20,13 +24,27 @@ bool verifyWokeUpTimes(void)
 void espSleep(uint32_t minutesSleep)
 {
     esp_sleep_enable_timer_wakeup(minutesSleep * 60 * uS_TO_S_FACTOR);
-
+    esp_sleep_pd_config(ESP_PD_DOMAIN_RTC_PERIPH,   ESP_PD_OPTION_OFF);
+    esp_sleep_pd_config(ESP_PD_DOMAIN_RTC_SLOW_MEM, ESP_PD_OPTION_ON);
+    esp_sleep_pd_config(ESP_PD_DOMAIN_RTC_FAST_MEM, ESP_PD_OPTION_OFF);
+    esp_sleep_pd_config(ESP_PD_DOMAIN_XTAL,         ESP_PD_OPTION_OFF);
+    esp_sleep_pd_config(ESP_PD_DOMAIN_RTC_PERIPH,   ESP_PD_OPTION_OFF);
+    
     Serial.println("Dormindo...");
     esp_deep_sleep_start();
+}
+
+void lightSleep(uint32_t milliSeconds)
+{
+    esp_sleep_enable_timer_wakeup(milliSeconds * uS_TO_mS_FACTOR);
+    
+    Serial.println("Light Sleep");
+    esp_light_sleep_start();
 }
 
 void gpsSleep(void)
 {
     digitalWrite(GPS_SLEEP, LOW);
-    Serial1.print("$PMTK161,0*28\r\n");
+    //Serial1.print("$PMTK161,0*28\r\n");//standby mode
+    Serial1.print("$PMTK225,4*2F\r\n");//backup mode
 }
